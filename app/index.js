0a7998d928859a9a9d29c42d895b26ddcbce2e51
Alejandro <soaresalejandro@outlook.com>
2021-12-01 17:00:26 -0300

    order assets in folders

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import api from "./helpers/wp_api.js";
-import { App } from "./App.js"
+import { App } from "./App.js";
 
 document.addEventListener("DOMContentLoaded", App);
 
-// Evento para detectar el cambio del hash
-window.addEventListener("hashchange", e => {
-    api.page = 1;
-    App()
+window.addEventListener("hashchange", (e) => {
+  api.page = 1;
+  App();
 });
